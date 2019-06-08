d3017da3b21acce933e4ef3e8298c9439f287966
LaZY <18846171630@163.com>
2019-06-08 22:32:54 +0800

    项目初始化

--- house_web/src/main/java/com/lazy/house/web/LazyHouseApplication.java
+++ house_web/src/main/java/com/lazy/house/web/LazyHouseApplication.java
@@ -1,9 +1,11 @@
 package com.lazy.house.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.lazy.house")
+@MapperScan("com.lazy.house.biz.mapper")
 public class LazyHouseApplication {
 
     public static void main(String[] args) {
